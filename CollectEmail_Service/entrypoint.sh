00a51b0459e58e32a4bcf0872f0a7b8f6692b9da
lak2611 <leanhkhoi2611@gmail.com>
2026-01-07 21:31:17 +0700

    Implement immediate email collection on container startup in entrypoint script
    - Added a command to run the email collection script upon container initialization, redirecting output to a log file for monitoring.

--- CollectEmail_Service/entrypoint.sh
+++ CollectEmail_Service/entrypoint.sh
@@ -3,6 +3,10 @@ set -e
 
 echo "Starting email collector container..."
 
+# Run immediately on container start
+echo "Running email collection on startup..."
+/app/run_email_and_upload.sh >> /var/log/cron.log 2>&1
+
 # Start cron daemon
 echo "Starting cron daemon..."
 cron
